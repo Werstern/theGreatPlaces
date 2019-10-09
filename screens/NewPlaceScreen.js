c353d4102cbbc1ee30e26078300dbe3e68613f84
Werstern <wersternn@gmail.com>
2019-10-09 13:46:12 +0300

    adding places

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -7,18 +7,23 @@ import {
   ScrollView, 
   StyleSheet 
 } from 'react-native';
+import { useDispatch } from 'react-redux';
 
 import Colors from '../constants/Colors';
+import * as placesActions from '../store/places-actions';
 
 const NewPlaceScreen = props => {
   const [titleValue, setTitleValue] = useState('');
+
+  const dispatch = useDispatch();
 
   const titleChangeHandler = text => {
     setTitleValue(text);
   };
 
   const savePlaceHandler = () => {
-    
+    dispatch(placesActions.addPlace(titleValue));
+    props.navigation.goBack();
   };
 
   return (
